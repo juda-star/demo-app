66a09264dd1bf290914522124a5c4d2ebf89400f
juda-star <juda.ishta@gmail.com>
2022-03-31 14:10:46 +0300

    first3

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/resume-angular'));
+app.use(express.static(path.dirname + '/dist/resume-angular'));
 app.get('/*', function(req,res)
-{res.sendFile(path.join(__dirname+'/dist/resume-angular/index.html'));});
+{res.sendFile(path.join(path.dirname+'/dist/resume-angular/index.html'));});
 app.listen(process.env.PORT || 8080);
